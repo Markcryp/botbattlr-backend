97adbe2295388088a135364bc2c630a502bc8fd0
EvrenTank <75124278+EvrenTank@users.noreply.github.com>
2023-09-22 13:56:33 +0300

    Update server.js

--- api/server.js
+++ api/server.js
@@ -7,8 +7,8 @@ const middlewares = jsonServer.defaults()
 server.use(middlewares)
 // Add this before server.use(router)
 server.use(jsonServer.rewriter({
-    '/api/*': '/$1',
-    '/blog/:resource/:id/show': '/:resource/:id'
+    '/api/flights': '/flights',
+   
 }))
 server.use(router)
 server.listen(3000, () => {
